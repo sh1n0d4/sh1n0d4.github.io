242a91576123171ad15f73d94a540ffe4b4fba35
sh1n0d4 <renshinoda168@gmail.com>
2021-11-02 10:04:19 +0000

    tailwind css 導入

---
+++ docs/_next/static/2w6cYYVHr66-eNzqiU2zN/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/04c21d679e6592b4.css","static/chunks/pages/index-a4f2a3ad46919130.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
